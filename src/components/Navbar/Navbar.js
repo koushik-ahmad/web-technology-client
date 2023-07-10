47685f4189651e19638de3fc24acea5e3a9330bd
koushik-ahmad <koushikahmad123@gmail.com>
2023-07-11 00:28:53 +0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const [state, setState] = useState(false)
+
+    return (
+        <nav className="bg-white border-b w-full md:static md:text-sm md:border-none">
+            <div className="items-center px-4 max-w-screen-2xl mx-auto md:flex md:px-8">
+                <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                    <div className='flex items-center'>
+                        <Link to='/'>
+                            <img
+                                src="https://i.ibb.co/2S83PPW/logo.png"
+                                width={70}
+                                height={40}
+                                alt="Float UI logo"
+                            />
+                        </Link>
+                        <Link to='/'>
+                            <h2 className='text-lg font-medium'>Web Technology</h2>
+                        </Link>
+
+                    </div>
+                    <div className="md:hidden">
+                        <button className="text-gray-500 hover:text-gray-800"
+                            onClick={() => setState(!state)}
+                        >
+                            {
+                                state ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                    </svg>
+                                ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+                                )
+                            }
+                        </button>
+                    </div>
+                </div>
+                <div className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${state ? 'block' : 'hidden'}`}>
+                    <ul className="justify-end items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+
+                        <div className='space-y-2 text-lg items-center gap-x-6 md:flex md:space-y-0'>
+
+                            <li>
+                                <Link to="/home" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/course' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Course
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/faq' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    FAQ
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/blog' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Blog
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/profile' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Profile
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/login' className="block py-1 px-2 text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow md:inline">
+                                    Log in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/signup' className="block py-1 px-2 text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow md:inline">
+                                    Sign Up
+                                </Link>
+                            </li>
+                            <li>
+                                <Link class="hs-dark-mode-active:hidden block hs-dark-mode group flex items-center text-gray-600 hover:text-blue-600 font-medium dark:text-gray-400 dark:hover:text-gray-500" href="#!" data-hs-theme-click-value="dark">
+                                    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
+                                    </svg>
+                                </Link>
+                                <Link class="hs-dark-mode-active:block hidden hs-dark-mode group flex items-center text-gray-600 hover:text-blue-600 font-medium dark:text-gray-400 dark:hover:text-gray-500" href="#!" data-hs-theme-click-value="light">
+                                    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                        <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                                    </svg>
+                                </Link>
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
---
+++ src/routes/router.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../layouts/Main';
+import Home from '../components/Home/Home';
+import Course from '../components/Course/Course';
+import Faq from '../components/Faq/Faq';
+import Blog from '../components/Blog/Blog';
+import Login from '../components/Register/Login';
+import Signup from '../components/Register/Signup';
+import Profile from '../components/Profile/Profile';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/course',
+                element: <Course></Course>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/profile',
+                element: <Profile></Profile>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+        ]
+    }
+]);
+
+export default router;
